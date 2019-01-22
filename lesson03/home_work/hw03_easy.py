15850302d4523ec4c5ebdbb6b45f32b6b504e709
Stanislav <staszzz@mail.ru>
2019-01-22 17:04:46 +0600

    lesson 3 easy S.Zabudkin

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,12 +5,22 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+   numberfloat = float(number)
+   numr = int(ndigits)
+   number1 = int(numberfloat*10**(ndigits+1))
+   print(" input plural to 10 and to numr+1",str(number1))
+   # delete
+   number3 = int(number1/10)*10
+   difn = number1-number3
+   print(str(difn))
+   # find difference
+   if (difn > 4):
+      number3 = (number3/10+1)/10**(ndigits)
+   number3 = (number3/10)/10**(ndigits)
+   print(" result ", str(number3))
 
+my_round(1234577.7,3)
 
-print(my_round(2.1234567, 5))
-print(my_round(2.1999967, 5))
-print(my_round(2.9999967, 5))
 
 
 # Задание-2:
@@ -20,9 +30,35 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+   if len(str(ticket_number)) < 6:
+      return "Error ticket number < 6"
+   num1 = int(ticket_number/10**3)
+   num2 = ticket_number - num1*10**3
+   print(" num 1", num1, " num2 ", num2 )
+
+  # print()
+
+   def calc_sum(num1):
+      s1 = 0
+      i = 0
+      while True :
+         i=i+1
+         res1 = int(num1/10**i)*10**i
+         dif = num1-res1
+         if i > 1:
+            dif = int(dif /10**(i-1))
+         if dif == num1:
+            s1 = s1 +dif;
+            break;
+         s1 = s1 +dif;
+         if res1 == 0:
+            break
+      return s1
+
+   s1 = calc_sum(num1)
+   s2 = calc_sum(num1)
+   print( " s1 ",s1, " s2 ", s2)
+
 
 
 print(lucky_ticket(123006))
-print(lucky_ticket(12321))
-print(lucky_ticket(436751))
